ca9038294512256bc2ba4e3910b7c410a483f23d
JeongGod <jeonggu223@gmail.com>
2020-08-04 21:15:40 +0900

    Stage 5.3 Import Stage 5.2 files

---
+++ Stage_5_3/app/src/main/java/com/example/stage_5_3/SportAdapter.java
@@ -0,0 +1,73 @@
+package com.example.stage_5_3;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class SportAdapter extends RecyclerView.Adapter<SportAdapter.ViewHolder> {
+
+    private ArrayList<Sport> mSportsData;
+    private Context mContext;
+
+    SportAdapter(Context context, ArrayList<Sport> sportsData) {
+        this.mSportsData = sportsData;
+        this.mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(mContext).inflate(R.layout.list_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Sport currentSport = mSportsData.get(position);
+        holder.bintTo(currentSport);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSportsData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private TextView mTitleText;
+        private TextView mInfoText;
+        private ImageView mSportsImage;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mTitleText = (TextView)itemView.findViewById(R.id.title);
+            mInfoText = (TextView)itemView.findViewById(R.id.subTitle);
+            mSportsImage = itemView.findViewById(R.id.sportsImage);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bintTo(Sport currentSport) {
+            mTitleText.setText((currentSport.getTitle()));
+            mInfoText.setText(currentSport.getInfo());
+            Glide.with(mContext).load(currentSport.getImageResource()).into(mSportsImage);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Sport currentSport = mSportsData.get(getAdapterPosition());
+            Intent detailIntent = new Intent(mContext, DetailActivity.class);
+            detailIntent.putExtra("title", currentSport.getTitle());
+            detailIntent.putExtra("image_resource", currentSport.getImageResource());
+            mContext.startActivity(detailIntent);
+        }
+    }
+}
