98952eea9ca2538146bcd988de3dad2da18a4c27
JeongGod <jeonggu223@gmail.com>
2020-08-04 19:12:46 +0900

    Stage 5.2 CardView : Android 지원 라이브러리에 존재한다. 각각의 View를 카드처럼 보여준다 Glide : 이미지로드 라이브러리. 이미지를 직접 로드하면 Memory Resource를 많이 잡아먹는다. 이미지를 효율적으로 로드하기위해 사용하는 라이브러리이다. Android지원 라이브러리는 아니다 ItemTouchHelper : Swipe, Drag and Drop등 이벤트에 대한 처리 FloatingActionBar(FAB) : Layout에 항상 띄워지는 Bar로 여러가지 기능을 추가할 수 있다

---
+++ Stage_5_2/app/src/main/java/com/example/stage_5_2/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.stage_5_2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private ArrayList<Sport> mSportData;
+    private SportAdapter mAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycleview);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mSportData = new ArrayList<>();
+        mAdapter = new SportAdapter(this, mSportData);
+        mRecyclerView.setAdapter(mAdapter);
+        initializeData();
+
+
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT
+            | ItemTouchHelper.UP | ItemTouchHelper.DOWN, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                int from = viewHolder.getAdapterPosition();
+                int to = target.getAdapterPosition();
+                Collections.swap(mSportData,from,to);
+                mAdapter.notifyItemMoved(from, to);
+                return true;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                mSportData.remove(viewHolder.getAdapterPosition());
+                mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+            }
+        });
+        helper.attachToRecyclerView(mRecyclerView);
+    }
+
+    private void initializeData() {
+        String[] sportslist = getResources().getStringArray(R.array.sports_titles);
+        String[] sportsinfo = getResources().getStringArray(R.array.sports_info);
+        TypedArray sportsImageResources = getResources().obtainTypedArray(R.array.sports_images);
+        mSportData.clear();
+        for(int i=0; i<sportslist.length; i++) {
+            mSportData.add(new Sport(sportslist[i], sportsinfo[i], sportsImageResources.getResourceId(i,0)));
+        }
+        sportsImageResources.recycle();
+        mAdapter.notifyDataSetChanged();
+    }
+
+    public void resetSports(View view) {
+        initializeData();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.put
+        super.onSaveInstanceState(outState);
+    }
+}
